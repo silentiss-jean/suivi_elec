3492562eb46e0c253d9036be7850258a111d9017
root <root@core-ssh.local.hass.io>
2025-09-09 17:15:09 +0200

    Auto release v2025.09.09-1715

---
+++ custom_components/suivi_elec/groupes_capteurs_energy.py
@@ -0,0 +1,12 @@
+# groupes_capteurs_energy.py
+
+groupes = [
+    {
+        "nom": "chauffage",
+        "capteurs": ["sensor.chauffage_1", "sensor.chauffage_2"]
+    },
+    {
+        "nom": "eclairage",
+        "capteurs": ["sensor.lumiere_salon", "sensor.lumiere_cuisine"]
+    }
+]
